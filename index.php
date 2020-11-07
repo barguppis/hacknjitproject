acc836782b313c1148c49879f979443e78edfc83
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-07 13:56:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ echo "Php can be run directly between html tags! Php code is executed by the ser
 ?>
 <!--To do add a button that executes this php code system("kill $(ps aux | grep -i php | awk '{print $2})")
 The button should be labeled something like, close program or exit, that's what this sytem command does, it kills the php server-->
-<iframe> src=/images/hacknjit.jpg</iframe>
+<iframe> src=http://localhost:2020/images/hacknjit.jpg</iframe>
