93bf58133fb991083ba4a63c5940be188a6d6905
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-07 15:33:12 -0500

    Update subdomains.sh

--- scripts/subdomains.sh
+++ scripts/subdomains.sh
@@ -2,3 +2,4 @@
 if [ ! -d scans ]; then mkdir scans;fi
 if [ ! -d scans/$1 ]; then mkdir scans/$1;fi
 wfuzz -f scans/$1/$1.out -w wordlists/directory-list-2.3-medium.txt --hc 404,400,401,402,403 -H "Host: FUZZ.$1" $1
+for line in $(cat scans/$1/$1.out | awk '{print $9}' | tr -d '"'); do echo $line.$1 >> scans/$1/$1.subdomains; done
