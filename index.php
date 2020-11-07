1bfd98b044cff38be04f927a3aa41a1b93aa401a
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-07 14:49:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,3 +6,11 @@ echo "Php can be run directly between html tags! Php code is executed by the ser
 <!--To do add a button that executes this php code system("kill $(ps aux | grep -i php | awk '{print $2})")
 The button should be labeled something like, close program or exit, that's what this sytem command does, it kills the php server-->
 <img src="/images/hacknjit.jpg" alt="This is a test">
+
+// Do you know how to make like text boxes to prompt for user input? also, php is pretty great. You can
+// do <?php system("./script.sh " . $_GET['argument']) ?>
+// you use periods to concatenate strings in php.
+// the above would run the system command ./script.sh and then whatever was sent in the "GET" request.
+// for example. http://localhost:2020/index.php?argument=yeedleyeedleyeedleyee
+// this would cause the system to execute "./script.sh yeedleyeedleyeedleyee"
+
