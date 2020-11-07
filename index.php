59decdd29dc2d2c7c59d0fcf252fadbb41d63f83
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-07 13:36:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 echo "Php can be run directly between html tags! Php code is executed by the server not the client\n"
 ?>
 <!--To do add a button that executes this php code system("kill $(ps aux | grep -i php | awk '{print $2})")
-The button should be labeled something like, close program or exit, that's what this sytem command does-->
+The button should be labeled something like, close program or exit, that's what this sytem command does, it kills the php server-->
