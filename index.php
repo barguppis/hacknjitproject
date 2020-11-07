e6f572f5ce98c1f7c5525a233ea28a7342980e59
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-07 16:11:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ echo "This application is meant to be run on your local host only and should nev
 The button should be labeled something like, close program or exit, that's what this sytem command does, it kills the php server-->
 <img src="/images/hacknjit.jpg" alt="This is a test">
 
-// Do you know how to make like text boxes to prompt for user input? also, php is pretty great. You can
-// do <?php system("./script.sh " . $_GET['argument']) ?>
-// you use periods to concatenate strings in php.
-// the above would run the system command ./script.sh and then whatever was sent in the "GET" request.
-// for example. http://localhost:2020/index.php?argument=yeedleyeedleyeedleyee
-// this would cause the system to execute "./script.sh yeedleyeedleyeedleyee"
+
+
+
+
+
+
 
 // subdomains.sh script is fininished, add a button or a page for subdomains. Button that takes to subdomains..
 // the script should be run like this <?php system("bash scripts/subdomains.sh " . USER_SUPPLIED_INPUT_AS_DOMAIN_TO_CHECK) ?>
