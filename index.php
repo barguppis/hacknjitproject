fbade2d3d15bcc4cfd5f062a861cf1ceb815971e
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-07 13:29:13 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 <?php
 echo "Php can be run directly between html tags! Php code is executed by the server not the client\n"
 ?>
+<!--To do add a button that executes this php code system("ps aux | grep -i php | awk '{print $1}' | kill") -->
--- notAutoEnum.py
+++ notAutoEnum.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 import os
-os.system("php -S 127.0.0.1:80 &")
+os.system("php -S 127.0.0.1:2020 &")
 os.system("disown")
+os.system("xdg-open http://localhost:2020")
