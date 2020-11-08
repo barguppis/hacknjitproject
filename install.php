20daec0e41bab252c7a1cdc8d7a563017f7cbec2
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-08 08:27:10 -0500

    Add files via upload

--- install.php
+++ install.php
@@ -1,5 +1,12 @@
 <title>Install Dependencies</title>
 <h1>Install Dependencies</h1>
+<p>Make sure you are running as the root user.</p>
+<form action="/install.php" method="post">
+	<input type="submit" value="Install" name="install"/>
 <?php
-system("bash scripts/install.sh");
+if(isset($_POST['install'])){
+system("bash scripts/install.sh > /dev/null");
+system("sleep 2");
+echo "<p>Install Script run successfully</p>";
+}
 ?>
