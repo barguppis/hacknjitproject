1f41dcbaeaebb22840098645dc11aacc38d8dd60
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-08 07:26:54 -0500

    Add files via upload

--- scripts/sublist3r.sh
+++ scripts/sublist3r.sh
@@ -2,3 +2,4 @@
 if [ ! -d scans ]; then mkdir scans;fi
 if [ ! -d scans/$1 ]; then mkdir scans/$1;fi
 sublist3r -d $1 -o scans/$1/$1.sublist3r > /dev/null
+cat scans/$1/$1.sublist3r >> scans/$1/$1.subdomains
