b5eeb1b42699101f93ab316a58b23f77b42ee639
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-08 11:47:25 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,23 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0";>
+  <title>About</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="css.css">
+</head>
+<body>
+  <section class="page-title">notAutoEnum > About</section>
+  <section class="text-block">
+    <p>notAutoEnum is an automatic enumeration toolkit. All back-end was written by Aidan Rivera, while front-end was written by Aaron Ha.</p>
+    <p>Programs used in its implementation include:</p>
+    <ul>
+      <li><a href="https://nmap.org/" target="_blank">nmap</li>
+      <li><a href="https://tools.kali.org/sniffingspoofing/responder" target="_blank">responder</li>
+      <li><a href="https://github.com/aboul3la/Sublist3r" target="_blank">sublist3r</li>
+      <li><a href="https://github.com/xmendez/wfuzz" target="_blank">wfuzz</li>
+      <li><a href="https://github.com/hashcat/hashcat" target="_blank">hashcat</li>
+    </ul>
+  </section>
+</body>
