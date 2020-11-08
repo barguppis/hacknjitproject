3b4632284af04bebf530ee9f21cdfde042cffa0c
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-08 11:08:41 -0500

    Add files via upload

--- scripts/hashcat.sh
+++ scripts/hashcat.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 hashcat -m 5600 -o responder/$1/$2.hashout --outfile-format 3 responder/$1/$2 wordlists/xato-net-10-million-passwords-1000000.txt
+hashcat -m 5600 responder/$1/$2 --show | awk -F ':' '{print $7}' > responder/$1/$2.hashout
