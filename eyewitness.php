2289a3f31c2d01f52dc662821dc971b4713f9447
Aidan Rivera <57341367+barguppis@users.noreply.github.com>
2020-11-08 11:13:22 -0500

    Add files via upload

---
+++ eyewitness.php
@@ -0,0 +1,9 @@
+<title>Eyewitness</title>
+<h1>We couldn't finish in time :)</h1>
+
+<form action="/eyewitness.php" method="post">
+	<label for="domain">yeet: </label> 
+	<input type="text" id="domain" name="domain"/>
+	<label for="args">salad: </label> 
+	<input type="text" id="args" name="args"/>
+	<input type="submit" value="Scan!"/>
